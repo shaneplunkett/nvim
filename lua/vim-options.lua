f438ea1b664118696ee3f8ea333ec8f5408312ea
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-01-29 22:50:53 +1100

    Update

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,66 +1,45 @@
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
 vim.g.have_nerd_font = true
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = 'a'
-
 vim.opt.showmode = false
-
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
-
 vim.opt.breakindent = true
-
 vim.opt.undofile = true
-
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.textwidth = 0
 vim.opt.wrap = true
 vim.opt.signcolumn = 'yes'
 vim.opt.colorcolumn = '81'
-
 vim.opt.updatetime = 250
-
 vim.opt.timeoutlen = 300
-
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
 vim.opt.inccommand = 'split'
-
 vim.opt.cursorline = true
-
 vim.opt.scrolloff = 999
-
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Exit insert mode' })
-
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -68,7 +47,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', { desc = 'Move to the window above' })
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', { desc = 'Move to the window below' })
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', { desc = 'Move to the window on the left' })
